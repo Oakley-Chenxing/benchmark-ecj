34a2b3a68682c94347683c2c5e3d147081b885f3
Mark Sinclair <markcsinclair@gmail.com>
2021-04-29 20:17:09 +0100

    Added sphere6

---
+++ src/ec/app/sphere/SphereProb6.java
@@ -0,0 +1,39 @@
+package ec.app.sphere;
+
+import ec.*;
+import ec.gp.*;
+import ec.gp.koza.*;
+import ec.simple.*;
+
+@SuppressWarnings("serial")
+public class SphereProb6 extends GPProblem implements SimpleProblemForm { 
+  @Override
+  public void evaluate(final EvolutionState state, 
+      final Individual ind,
+      final int subpopulation,
+      final int threadnum) {
+    if (!ind.evaluated) { // don't bother reevaluating
+      float fit  = 0f;
+      int   hits = 0;
+      for (int t=0; t<DIM; t++) {
+        ((GPIndividual)ind).trees[t].child.eval
+        (state, threadnum, input, stack,
+            ((GPIndividual)ind), this);
+        float x = ((SphereData)input).x;
+        x = Math.max(x, -1f);
+        x = Math.min(x, 1f);
+        if (Math.abs(x) <= 1e-8f)
+          hits++;
+        fit += x*x;
+      }
+
+      // the fitness better be KozaFitness!
+      KozaFitness f = ((KozaFitness)ind.fitness);
+      f.setStandardizedFitness(state, (double)(fit));
+      f.hits        = hits;
+      ind.evaluated = true;
+    }
+  }
+  
+  public static final int DIM = 6;
+}
--- src/ec/app/sphere/SphereStats6.java
+++ src/ec/app/sphere/SphereStats6.java
@@ -5,7 +5,7 @@ import ec.gp.*;
 import ec.simple.*;
 
 @SuppressWarnings("serial")
-public class SphereStats2 extends SimpleStatistics {
+public class SphereStats6 extends SimpleStatistics {
   @Override
   public void finalStatistics(final EvolutionState state, final int result) {
     super.finalStatistics(state,result);
@@ -14,7 +14,7 @@ public class SphereStats2 extends SimpleStatistics {
       System.out.print("[ ");
       GPData       input = new SphereData();
       GPIndividual ind   = (GPIndividual)best_of_run[0];
-      for (int t=0; t<SphereProb2.DIM; t++) {
+      for (int t=0; t<SphereProb6.DIM; t++) {
         ind.trees[t].child.eval(state, 0, input, null, ind, null);
         float x = ((SphereData)input).x;
         x = Math.max(x, -1f);
