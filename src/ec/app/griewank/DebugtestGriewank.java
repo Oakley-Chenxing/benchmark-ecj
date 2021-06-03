779ee4bbbc77b0e44cbb989cb8446441f2b93faf
Qakley-Chenxing <Chenxing.li@northumbria.ac.uk>
2021-06-03 17:39:25 +0800

    0603 correct griwank

--- src/ec/app/griewank/DebugtestGriewank.java
+++ src/ec/app/griewank/DebugtestGriewank.java
@@ -1,22 +1,27 @@
 package ec.app.griewank;
-
 public class DebugtestGriewank {
 	public static void main(String[] args) {
-	    float[] rep = new float[] { 1.414f, 0.800f };
+	    float[] rep = new float[] { 0f, 0f };
 	    float   gri = griewank(rep);
 	    System.out.println("gri: "+gri);
 	  }
 	 public static float griewank(float[] rep) {
 		 //griewank code reference:
 		 //http://www.sfu.ca/~ssurjano/griewank.html
-		 float sum = 0.0f;
-         float d = 1.00f;
-		   for (int i=1; i<rep.length; i++) {
+		 float sum  = 0.0f;
+		 float prod = 1.0f;
+		   for (int i=0; i<rep.length; i++) {
+		       System.out.println("rep["+i+"]: "+rep[i]);
 			   float temp1 = rep[i]*rep[i];
-			   float temp2 = (float) (Math.PI * (Math.cos(rep[i]/Math.sqrt(i))));
-			   sum += (1/4000)*temp1 - (temp2)+ d;
+			   System.out.println("temp1: "+temp1);
+			   sum += temp1;
+			   System.out.println("sum: "+sum);
+			   float temp2 = (float) (Math.cos(rep[i]/Math.sqrt(i+1)));
+			   System.out.println("temp2: "+temp2);
+			   prod *= temp2;
+			   System.out.println("prod: "+prod);
 	 }
-			return sum;
+			return (1/4000)*sum - prod + 1.0f;
 }
 	 public static final int DIM = 2; 
 
